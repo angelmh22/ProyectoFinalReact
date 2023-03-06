159c3fc45093fc770b9f8567d301d228c78550d6
angelmh22 <angelmartinezhu@gmail.com>
2023-03-06 20:24:06 +0100

    estructura del proyecto pero sin codigo cambiado

--- src/components/Error-page.jsx
+++ src/components/Error-page.jsx
@@ -5,8 +5,8 @@ export default function ErrorPage() {
   const error = useRouteError();
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>¡Vaya!</h1>
+      <p>Lo siento, se ha producido un error.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
